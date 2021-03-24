ad5ab309dfc20c96ffdad6e630fe1927c6ff6009
Gustavo Vilela <gutusmao12@hotmail.com>
2021-03-24 17:02:03 -0300

    Implementando menu inicial

--- Front-End/src/app/model/UserLogin.ts
+++ Front-End/src/app/model/UserLogin.ts
@@ -1,7 +1,7 @@
 export class UserLogin{
     public id: number
     public nome: string
-    public usuaroi: string
+    public usuario: string
     public senha: string
     public token: string
     public foto: string
